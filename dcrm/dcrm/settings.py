950637f168420702083bc3f2fda616cb8eca3fda
Hrishabh Kumar <hrishabh068@gmail.com>
2025-08-12 08:51:05 +0530

    Update settings.py

--- dcrm/dcrm/settings.py
+++ dcrm/dcrm/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'dcrm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'website','templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
